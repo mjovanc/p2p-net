0d6bda81892c61b6191ea54f3afeaad60398e470
mjovanc <mjovanc@icloud.com>
2023-04-07 20:50:23 +0200

    Adding loop for chat input

---
+++ src/client.rs
@@ -0,0 +1,4 @@
+
+pub(crate) fn send_message(message: String) {
+    println!("Sending message: {}", message);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ mod client;
 
 fn main() {
     server::start();
+
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        client::send_message(input);
+    }
 }
