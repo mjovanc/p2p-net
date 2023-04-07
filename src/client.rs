547033e78510486404788a90e840ed0735846022
mjovanc <mjovanc@icloud.com>
2023-04-07 20:57:19 +0200

    Updating client.rs

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,19 @@
+use reqwest::blocking::Client;
 
 pub(crate) fn send_message(message: String) {
     println!("Sending message: {}", message);
+
+    let client = Client::new();
+    let res = client.get("http://localhost:8477").send();
+
+    match res {
+        Ok(response) => {
+            if response.status().is_success() {
+                println!("Server response: {:?}", response.text());
+            } else {
+                println!("Server returned error: {}", response.status());
+            }
+        },
+        Err(e) => println!("Error sending request: {}", e),
+    }
 }
